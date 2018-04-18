0e6715e2558ebf4ec27faf675e8ea61079b38d6e
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2018-04-18 11:27:12 +0200

    removed modal

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../models/user';
 import { GeoService } from '../geo.service';
 
@@ -11,14 +10,13 @@ import { GeoService } from '../geo.service';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  private usersCollection: AngularFirestoreCollection<User>;  
   users: Observable<any[]>;  
   dbRef: any;
-  closeResult: string;
-  private usersCollection: AngularFirestoreCollection<User>;
   usersData = [];
+  userForEditing: User;
 
   constructor(
-    private modalService: NgbModal,
     private afs: AngularFirestore, 
     private geoService: GeoService
   ) {
@@ -53,24 +51,6 @@ export class UserListComponent implements OnInit {
   calculateDistance() {
     let distances = this.geoService.calculateDistance(this.usersData);
     alert(JSON.stringify(distances, null, 4));
-  }
-
-  open(content) {
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
   }
 
 }
