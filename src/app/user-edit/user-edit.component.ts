2479161dfcdcbf43f9768d6874af4ee4cc70e204
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2018-04-20 14:05:06 +0200

    load user on user edit page

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import { MapsAPILoader } from '@agm/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { User } from '../models/user';
 
@@ -9,16 +11,60 @@ import { User } from '../models/user';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+  public searchControl: FormControl;
   user: any;
+  userId: any;
+  errorFields = [];
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
-    private afs: AngularFirestore 
-  ) { }
+    private afs: AngularFirestore,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
+
+  initUser() {
+    this.user = new User('', '', {
+      name: '',
+      latitude: null,
+      longitude: null
+    });
+  }
+
+  initGooglePlaces() {
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: []
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place = autocomplete.getPlace();
+  
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          this.user.location.latitude = place.geometry.location.lat();
+          this.user.location.longitude = place.geometry.location.lng();
+          this.user.location.name = place.name;
+          
+        });
+      });
+    });
+  }
 
   ngOnInit() {
+    this.searchControl = new FormControl();
+    this.initUser();
+    this.initGooglePlaces();
     this.route.params.subscribe( params => {
-      const user: any = this.afs.doc<User>('users/' + params.id)
+      this.userId = params.id;
+      const user: any = this.afs.doc<User>('users/' + this.userId)
       .snapshotChanges()
       .map(actions => {
         return actions.payload.data() as User;
@@ -27,6 +73,10 @@ export class UserEditComponent implements OnInit {
         this.user = userData as User;
       });
     });
+  }
+
+  cancel() {
+    this.router.navigate(['']);
   }
 
 }
