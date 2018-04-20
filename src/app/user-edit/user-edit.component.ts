b5c496b3df8cb019cdf55b750966c0ec03eba099
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2018-04-20 13:46:23 +0200

    load user data async on (user edit page)

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private afs: AngularFirestore 
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe( params => {
+      const user: any = this.afs.doc<User>('users/' + params.id)
+      .snapshotChanges()
+      .map(actions => {
+        return actions.payload.data() as User;
+      });
+      user.subscribe(userData => {
+        this.user = userData as User;
+      });
+    });
   }
 
 }
