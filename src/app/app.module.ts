7aaaae0b36361118dcdbf151041a49b95830883d
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2018-04-20 13:10:58 +0200

    added routing and edit user route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { GooglePlaceModule } from "ngx-google-places-autocomplete";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -15,15 +16,27 @@ import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { GoogleMapComponent } from './google-map/google-map.component';
 import { GeoService } from './geo.service';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'edit-user/:id', component: UserEditComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    UserEditComponent,    
     UserListComponent,
-    GoogleMapComponent
+    GoogleMapComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     AgmCoreModule.forRoot({
       apiKey: environment.googleMapsKey,
       libraries: ["places"]
